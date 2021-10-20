19810adc16ed100f6990b1aff2512caca29aa80c
jimmyhzhao <jimmyhzhao@tencent.com>
2021-10-20 20:27:28 +0800

    update port from 3000 to 30002

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   return res.send('Hello world');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30002;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
